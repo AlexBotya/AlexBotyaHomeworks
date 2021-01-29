8e6504e92e25161e5067d9032d74a20f51b2bb28
AlexBotya <alex_balg@bk.ru>
2021-01-29 22:10:26 +0300

    Println 'My 1 lesson'

--- src/Lesson1/Lesson1.java
+++ src/Lesson1/Lesson1.java
@@ -1,8 +1,30 @@
 package Lesson1;
 
 public class Lesson1 {
-    public static void main (String[] args){
-        System.out.println("My 1 Lesson");
+    public static void main(String[] args) {
+        System.out.println("My Lesson 1");
+        byte a1 = 127;
+        short a2 = 32767;
+        int a3 = 999999;
+        long a4 = 12345L;
+        float a5 = 98765.01f;
+        double a6 = -100000.432;
+        char a7 = 'c';
+        boolean a8 = true;
+        System.out.println("byte=" + a1);
+        System.out.println("short=" + a2);
+        System.out.println("int " + a3);
+        System.out.println("long " + a4);
+        System.out.println("float " + a5);
+        System.out.println("double " + a6);
+        System.out.println("char " + a7);
+        System.out.println("boolean " + a8);
+
+
+
+
+
+
 
     }
 }
