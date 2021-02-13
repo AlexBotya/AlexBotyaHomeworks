fec8ca5958ebc9d5c306e5d8399988a7d5fa0a0a
AlexBotya <alex_balg@bk.ru>
2021-02-14 00:08:31 +0300

    Lesson 5 fixed by teacher's remarks (class main consist call 1 method only)

--- src/lesson5/lesson5.java
+++ src/lesson5/lesson5.java
@@ -2,22 +2,15 @@ package lesson5;
 
 public class lesson5 {
     public static void main(String[] args) {
+//получилось!! Main из одной строки)))
 
-        worker workers[] = new worker[5];
-        workers[0]=new worker("Ivan","Engineer", "ivan@mail.com", "+123", 2000, 31);
-        workers[1]=new worker("Maks","Engineer", "max@mail.com", "+134", 4000, 45);
-        workers[2]=new worker("Dan","Director", "dir@mail.com", "+111", 10000, 56);
-        workers[3]=new worker("Yana","Office-manager", "yana@mail.com", "+133", 1300, 22);
-        workers[4]=new worker("Anna","HR director", "HR@mail.com", "+343", 6000, 41);
-
-
-        checkAge40(workers);
+        checkAge40(createWorkersArray());
 
     }
     public static void checkAge40(worker workers[]){
         System.out.println("Список сотрудников старше 40 лет:\n");
         for (int i = 0; i <workers.length ; i++) {
-            if (workers[i].age > 40){
+            if (workers[i].getAge() > 40){
                 workers[i].workerDataPrint();
             }
 
@@ -25,16 +18,15 @@ public class lesson5 {
 
     }
 
-    /*метод не работает. пока не работает
-    public static worker[] createWorkersArray(){
+    public static worker[]  createWorkersArray(){
         worker workersArray[] = new worker[5];
         workersArray[0]=new worker("Ivan","Engineer", "ivan@mail.com", "+123", 2000, 31);
-        workers[1]=new worker("Maks","Engineer", "max@mail.com", "+134", 4000, 45);
-        workers[2]=new worker("Dan","Director", "dir@mail.com", "+111", 10000, 56);
-        workers[3]=new worker("Yana","Office-manager", "yana@mail.com", "+133", 1300, 22);
-        workers[4]=new worker("Anna","HR director", "HR@mail.com", "+343", 6000, 41);
-        return workers[];
-    }*/
+        workersArray[1]=new worker("Maks","Engineer", "max@mail.com", "+134", 4000, 45);
+        workersArray[2]=new worker("Dan","Director", "dir@mail.com", "+111", 10000, 56);
+        workersArray[3]=new worker("Yana","Office-manager", "yana@mail.com", "+133", 1300, 22);
+        workersArray[4]=new worker("Anna","HR director", "HR@mail.com", "+343", 6000, 41);
+        return workersArray;
+    }
 
 }
 
--- src/lesson5/worker.java
+++ src/lesson5/worker.java
@@ -1,12 +1,12 @@
 package lesson5;
 
 public class worker {
-    public String name;
-    public String post;
-    public String email;
-    public String mobile;
-    public int salary;
-    public int age;
+    private String name;
+    private String post;
+    private String email;
+    private String mobile;
+    private int salary;
+    private int age;
 
     public worker (String name, String post, String email, String mobile, int salary, int age){
         this.name = name;
@@ -19,5 +19,8 @@ public class worker {
     public void workerDataPrint(){
         System.out.printf("Имя: %s%n Должность: %s%n email: %s%n телефон: %s%n оклад: %s%n возраст: %s%n",
                 name, post,email, mobile, salary, age);
+    }
+    public int getAge(){
+        return age;
     }
 }
